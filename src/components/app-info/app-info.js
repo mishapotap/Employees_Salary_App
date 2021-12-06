98a3a4ea2df147106214ebd2e4bd298528d92518
Mikhail Potapov <86321783+mishapotap@users.noreply.github.com>
2021-12-06 23:48:21 +0300

    Employees_and_increased_done

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,12 +1,12 @@
 // App header
 import "./app-info.css";
 
-const AppInfo = () => {
+const AppInfo = ({ employees, increased }) => {
     return (
         <div className="app-info">
             <h1>Учет сотрудников в компании "N"</h1>
-            <h2>Общее число сотрудников: __</h2>
-            <h2>Премию получат: __ человек</h2>
+            <h2>Общее число сотрудников: {employees}</h2>
+            <h2>Премию получат: {increased} человек</h2>
         </div>
     );
 };
