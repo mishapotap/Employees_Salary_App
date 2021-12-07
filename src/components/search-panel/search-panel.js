b41c1828ce5b3af34e8d597413677135fe9d4a90
Mikhail Potapov <86321783+mishapotap@users.noreply.github.com>
2021-12-07 22:33:52 +0300

    Search_added

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,8 +1,30 @@
 // Search
+import { Component } from "react";
 import "./search-panel.css";
 
-const SearchPanel = () => {
-    return <input type="text" className="form-control search-input" placeholder="Найти сотрудника" />;
-};
+class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: "",
+        };
+    }
+    onUpdateSearchLocal = (event) => {
+        const term = event.target.value;
+        this.setState({ term });
+        this.props.onUpdateSearch(term);
+    };
+    render() {
+        return (
+            <input
+                type="text"
+                className="form-control search-input"
+                placeholder="Найти сотрудника"
+                value={this.state.term}
+                onChange={this.onUpdateSearchLocal}
+            />
+        );
+    }
+}
 
 export default SearchPanel;
